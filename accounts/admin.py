c9da3635bb9e5f37ba2641155687cf96957e0783
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-05-27 20:17:21 +0000

    Created Custom User, Reviewer

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Reviewer
 
-# Register your models here.
+class ReviewerInline(admin.StackedInline):
+    model = Reviewer
+    # can_delete = False
+    # verbose_name_plural = 'user'
+class UserAdmin(BaseUserAdmin):
+    inlines = (ReviewerInline,)
+
+# admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
