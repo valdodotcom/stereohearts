eae40f1cf186ca3c472ea2c472201f23bdab6c6d
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-06-05 22:42:50 +0000

    Work on Frontend (unfinished)

--- frontend/views.py
+++ frontend/views.py
@@ -7,32 +7,23 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 
 from django.db.models import Q
+import requests
+
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
 
 # Create your views here.
 def login_page(request):
     page = 'login'
-    if request.user.is_authenticated:
-        return redirect('home')
-    
+    STEREOHEARTS_URL = environ.get('STEREOHEARTS_URL')
+
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
-        password = request.POST.get('password')
+        response = requests.post('https://api.example.com/endpoint', data={'key': 'value'})
+        if response.status_code == 200:
+            return redirect('home') 
 
-        try:
-            user = Reviewer.objects.get(username=username)
-        except:
-            messages.error(request, 'User does not exist')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        
-        else:
-            messages.error(request, 'Invalid username or password')
-
-    context = {'page': page}
+    context = {'page': page, 'link':STEREOHEARTS_URL}
     return render(request, 'frontend/login_register.html', context)
 
 def logout_user(request):
@@ -41,65 +32,20 @@ def logout_user(request):
 
 def register_page(request):
     page = 'register'
-    form = UserForm()
 
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            login(request, user)
-            return redirect('create-reviewer')
-        else:
-            messages.error(request, 'An error occured during registration')
-
-    context = {'page': page, 'form': form}
+    context = {'page': page}
     return render(request, 'frontend/login_register.html', context)
 
 @login_required(login_url='login')
 def create_reviewer(request):
     page = 'create-reviewer'
 
-    if request.method == 'POST':
-        form = ReviewerForm(request.POST)
-        if form.is_valid():
-            reviewer = form.save(commit=False)
-            if reviewer.user != request.user:
-                return HttpResponseForbidden("You are not authorized to create a reviewer for this user.")
-            reviewer.save()
-            return redirect('home')
-        else:
-            messages.error(request, 'An error occurred during registration')
-    else:
-        form = ReviewerForm(initial={'user': request.user})
-
-    context = {'page': page, 'form': form}
+    context = {'page': page}
     return render(request, 'frontend/create_reviewer.html', context)
 
 def home(request):
-    # search
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    users = Reviewer.objects.filter(
-        Q(user__username__icontains=q) |
-        Q(display_name__icontains=q)
-        )
-    
-    # topics = Topic.objects.all()
-    # room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
-
-    context = {'users': users}
-    return render(request, 'frontend/home.html', context)
+    return render(request, 'frontend/home.html')
 
 @login_required(login_url='login')
 def update_user(request):
-    user = request.user
-    form = UserForm(instance=user)
-
-    if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect('user-profile', pk=user.id)
-
-    context = {'form': form}
-    return render(request, 'frontend/update_user.html', context)
+    return render(request, 'frontend/update_user.html')
