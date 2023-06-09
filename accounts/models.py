de08d8e7c1f12b5ad0a0c8137a3c8356f9962cec
Yoocee Ansah <yooceeansah@gmail.com>
2023-06-09 18:22:04 +0100

    Linked the User model to the Models in the other models.py files

--- accounts/models.py
+++ accounts/models.py
@@ -5,21 +5,28 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
   email = models.EmailField(unique=True, null=False)
+
+  #assuming the vast majority of users will be reviewers
+  is_reviewer = models.BooleanField(default=True)
+  is_moderator = models.BooleanField(default=False)
+
+  #I'll leave you to add any more fields you want, especially those from the Reviewer class below  
   
   def __str__(self):
     return self.username
 
-class Reviewer(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-  display_name = models.CharField(max_length=200, null=True)
-  bio = models.TextField(blank=True, null=True)
-  # profile_picture = models.ImageField(upload_to='reviewer_profiles/', blank=True)
-  # reviews = models.OneToManyField(Review, related_name='reviewers')
-  # lists = models.OneToManyField(List, related_name='reviewers')
-  # likes =  models.OneToManyField(Like, related_name='reviewers')
-  # favorite_project = models.ManyToManyField('Project', related_name='reviewers')
-  # favorite_artists = models.ManyToManyField('Artist', related_name='reviewers')
-  # score = models.CharField(max_length=200, null=True)
+# class Reviewer(models.Model):
 
-  def __str__(self):
-    return self.user.username
+#   user = models.ForeignKey(User, on_delete=models.CASCADE)
+#   display_name = models.CharField(max_length=200, null=True)
+#   bio = models.TextField(blank=True, null=True)
+#   # profile_picture = models.ImageField(upload_to='reviewer_profiles/', blank=True)
+#   # reviews = models.OneToManyField(Review, related_name='reviewers')
+#   # lists = models.OneToManyField(List, related_name='reviewers')
+#   # likes =  models.OneToManyField(Like, related_name='reviewers')
+#   # favorite_project = models.ManyToManyField('Project', related_name='reviewers')
+#   # favorite_artists = models.ManyToManyField('Artist', related_name='reviewers')
+#   # score = models.CharField(max_length=200, null=True)
+
+#   def __str__(self):
+#     return self.user.username
--- spotify/models.py
+++ spotify/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-from accounts.models import Reviewer
+from accounts.models import User
 class SpotifyToken(models.Model):
-    user = models.ForeignKey(Reviewer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #changed from Reviewer to User
     spotify_user = models.CharField(max_length=50, unique=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     refresh_token = models.CharField(max_length=150,  null=True)
