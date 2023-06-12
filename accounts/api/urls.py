8a1848927d2d1bc1fe6bcfdf29547be5080927c9
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-06-12 20:42:47 +0000

    added obtain_auth_token function

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import *
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('', getRoutes),
+    path('auth/', obtain_auth_token),
+
     path('users/', UserView.as_view()),
     path('login/', loginUser),
     path('logout/', logoutUser),
--- stereohearts/settings.py
+++ stereohearts/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'frontend.apps.FrontendConfig',
 
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -135,3 +136,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication"
+    ],
+}
