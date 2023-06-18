11576e62cbc7066adaeabcdfb8422761e1958ff8
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-06-18 20:42:30 +0000

    added pagination

--- stereohearts/settings.py
+++ stereohearts/settings.py
@@ -143,4 +143,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication"
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 5,
 }
