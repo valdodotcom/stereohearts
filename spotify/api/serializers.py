0b10f536e9c1df1ea4fcdd01fbbb94442d8706ba
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-07-08 17:08:17 +0000

    changed artist url to id

--- spotify/api/serializers.py
+++ spotify/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 class ArtistSerializer(serializers.Serializer):
-    external_urls = serializers.DictField()
+    id = serializers.CharField()
     name = serializers.CharField()
 
 class ParsedAlbumSerializer(serializers.Serializer):
