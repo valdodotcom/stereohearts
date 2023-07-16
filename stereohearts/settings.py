af44ddfc08d15727c2641468fd779309e6e17274
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-07-16 03:22:21 +0000

    changed length of jwt tokens changed cors allowed origins to frontend_url

--- stereohearts/settings.py
+++ stereohearts/settings.py
@@ -74,7 +74,7 @@ CORS_ALLOWED_ORIGINS = []
 
 if DEBUG:
     CORS_ALLOWED_ORIGINS += [
-        environ.get('CORS_ALLOWED_ORIGINS'),
+        environ.get('FRONTEND_URL'),
     ]
 
 TEMPLATES = [
@@ -169,6 +169,6 @@ ALGOLIA = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ["Bearer"],
-    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=2),
-    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=7),
 }
